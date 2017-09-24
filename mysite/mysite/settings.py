2c4d2902b5d3e6d17579933abdab22a5c3d08c09
Harshit Kumar <kumar_harshit@outlook.com>
2017-09-24 20:16:19 +0530

    fix: Add host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1r8jv((i8@s*q)nof2r_n^cc+d6)dv)6h@mpmq(-+w(+56*%w5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']  # ['127.0.0.1', '.pythonanywhere.com'] added
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'harshitblog.herokuapp.com']  # ['127.0.0.1', '.pythonanywhere.com'] added
 
 
 # Application definition
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',  # 'django.db.bakckends.sqlite3' added
         'NAME': 'myblog',  # os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': 'harshit',
-        'PASSWORD': 'goodboy@123',
+        'PASSWORD': 'helloworld',
         'HOST': 'localhost',
         'PORT': '',
     }
