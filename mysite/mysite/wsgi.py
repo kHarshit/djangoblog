0edebcfa7a190930a44dbd8ba1cf6b83b5e93784
Harshit Kumar <kumar_harshit@outlook.com>
2017-09-24 20:39:02 +0530

    Add heroku wsgi app path

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+
+# path for herokuapp
+path = "/home/harshitblog/myblog/mysite"
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
