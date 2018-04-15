d7472f75de66b046e0494539b59987fe205442d3
Harshit Kumar <kumar_harshit@outlook.com>
2018-04-15 22:13:39 +0530

    Configure postgres for kali dell

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1r8jv((i8@s*q)nof2r_n^cc+d6)dv)6h@mpmq(-+w(+56*%w5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
